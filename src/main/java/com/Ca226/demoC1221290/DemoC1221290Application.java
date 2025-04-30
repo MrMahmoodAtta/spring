69495ac6e1dbcbdbc56ffcb535d5944534044b30
MrMahmoodAtta <163418952+MrMahmoodAtta@users.noreply.github.com>
2025-04-30 12:55:16 +0300

    add all files

---
+++ src/main/java/com/Ca226/demoC1221290/DemoC1221290Application.java
@@ -0,0 +1,13 @@
+package com.Ca226.demoC1221290;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoC1221290Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoC1221290Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/Ca226/demoC1221290/StaffController.java
@@ -0,0 +1,39 @@
+package com.Ca226.demoC1221290;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/Staff")
+@AllArgsConstructor  // Only this is needed
+public class StaffController {
+
+    private final StaffService staffService;
+
+    @GetMapping()
+    public Collection<Staff> getAllStaffs() {
+        return staffService.getAllStaffs();
+    }
+
+    @PostMapping()
+    public void addStaff(@RequestBody Staff newStaff) {
+        staffService.createStaff(newStaff);
+    }
+
+    @GetMapping("/{id}")
+    public Staff getStaffById(@PathVariable Long id) {
+        return staffService.getStaffById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Staff updateStaffById(@PathVariable Long id, @RequestBody Staff newStaff) {
+        return staffService.UpdateStaff(id, newStaff);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStaff(@PathVariable Long id) {
+        staffService.deleteStaff(id);
+    }
+}
